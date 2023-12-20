49b35623e0065c2fa8bf97f445cf12835d0cc6ca
ankitthakur7 <ankitkushwahavps_cse22@gmail.com>
2023-12-21 03:40:34 +0530

    Add some initial file

---
+++ font.java
@@ -0,0 +1,12 @@
+class FF{
+    int x = 10;
+    public void show(){
+        System.out.println(x);
+    }
+}
+public class font{
+    public static void main(String[] args) {
+        FF f = new FF();
+        f.show();
+    }
+}
